818cbc372d0a0b29334e9feeec7ccb07bd81d6eb
Jajovise-11 <148467900+Jajovise-11@users.noreply.github.com>
2025-05-09 13:45:15 +0200

    popo1

--- Enredados/src/app/components/principal/principal.component.ts
+++ Enredados/src/app/components/principal/principal.component.ts
@@ -1,6 +1,7 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-principal',
@@ -10,13 +11,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
-    users = [
-      { name: 'Carmen', text: 'BLABLABLA...', avatar: 'https://i.pravatar.cc/40?img=1' },
-      { name: 'Jose', text: 'BLBLBLBLBLA...', avatar: 'https://i.pravatar.cc/40?img=2' },
-      { name: 'Mónica', text: 'blablablabla...', avatar: 'https://i.pravatar.cc/40?img=3' },
-      { name: 'Adrián', text: 'BLBLABLABL', avatar: 'https://i.pravatar.cc/40?img=4' },
-      { name: 'Paula', text: 'BALABLLBLABLA', avatar: 'https://i.pravatar.cc/40?img=5' },
-      { name: 'María', text: 'BLABLABLABLABL', avatar: 'https://i.pravatar.cc/40?img=6' }
-    ];
+  users = [
+    { name: 'Carmen', text: 'BLABLABLA...', avatar: 'https://i.pravatar.cc/40?img=1' },
+    { name: 'Jose', text: 'BLBLBLBLBLA...', avatar: 'https://i.pravatar.cc/40?img=2' },
+    { name: 'Mónica', text: 'blablablabla...', avatar: 'https://i.pravatar.cc/40?img=3' },
+    { name: 'Adrián', text: 'BLBLABLABL', avatar: 'https://i.pravatar.cc/40?img=4' },
+    { name: 'Paula', text: 'BALABLLBLABLA', avatar: 'https://i.pravatar.cc/40?img=5' },
+    { name: 'María', text: 'BLABLABLABLABL', avatar: 'https://i.pravatar.cc/40?img=6' }
+  ];
+
+  currentSlide = 0;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setInterval(() => {
+        this.currentSlide = (this.currentSlide + 1) % 3;
+      }, 4000);
 }
+}
+}
+
 
