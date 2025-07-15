b8f58d711ca3ff3cd9fe9fc18617e4387408e109
Jajovise-11 <148467900+Jajovise-11@users.noreply.github.com>
2025-07-15 12:43:53 +0200

    duro

--- Enredados/src/app/components/principal/principal.component.ts
+++ Enredados/src/app/components/principal/principal.component.ts
@@ -19,6 +19,60 @@ export class PrincipalComponent {
     { name: 'Paula', text: 'BALABLLBLABLA', avatar: 'https://i.pravatar.cc/40?img=5' },
     { name: 'María', text: 'BLABLABLABLABL', avatar: 'https://i.pravatar.cc/40?img=6' }
   ];
+  bodas = [
+    {
+      pareja: 'Adriana & Sergio',
+      imagenPrincipal: 'https://via.placeholder.com/300x200?text=Adriana+Sergio',
+      miniaturas: [
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40'
+      ],
+      fotos: 97,
+      lugar: 'Oviedo, Asturias'
+    },
+    {
+      pareja: 'Carla & Juan',
+      imagenPrincipal: 'https://via.placeholder.com/300x200?text=Carla+Juan',
+      miniaturas: [
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40'
+      ],
+      fotos: 90,
+      lugar: 'Malleza, Asturias'
+    },
+    {
+      pareja: 'Sara & Adrián',
+      imagenPrincipal: 'https://via.placeholder.com/300x200?text=Sara+Adrian',
+      miniaturas: [
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40'
+      ],
+      fotos: 68,
+      lugar: 'Gijón, Asturias'
+    },
+    {
+      pareja: 'Sofía & Santi',
+      imagenPrincipal: 'https://via.placeholder.com/300x200?text=Sofia+Santi',
+      miniaturas: [
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40',
+        'https://via.placeholder.com/60x40'
+      ],
+      fotos: 48,
+      lugar: 'Onon (Cangas De Narcea), Asturias'
+    }
+  ];
+  ideas = [
+    { titulo: 'Antes de la boda', imagen: 'https://via.placeholder.com/100?text=Antes' },
+    { titulo: 'La ceremonia de la boda', imagen: 'https://via.placeholder.com/100?text=Ceremonia' },
+    { titulo: 'El banquete', imagen: 'https://via.placeholder.com/100?text=Banquete' },
+    { titulo: 'Los servicios para tu boda', imagen: 'https://via.placeholder.com/100?text=Servicios' },
+    { titulo: 'Moda nupcial', imagen: 'https://via.placeholder.com/100?text=Moda' },
+    { titulo: 'Belleza y salud', imagen: 'https://via.placeholder.com/100?text=Belleza' }
+  ];
 
   currentSlide = 0;
 
