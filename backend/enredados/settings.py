e98a66148ebc66af9781686bb602c2e9bfad04ce
Jajovise-11 <jmo.ideas@gmail.com>
2025-12-03 16:49:12 +0100

    pimpampum

--- backend/enredados/settings.py
+++ backend/enredados/settings.py
@@ -83,12 +83,28 @@ WSGI_APPLICATION = 'enredados.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# COMENTA la configuración de MySQL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'enredados_db',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
+# USA SQLite temporalmente
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
